b259f51f6c9e832b7dd698773847944c96c2a42e
stanislavtiryoshin <69424907+stanislavtiryoshin@users.noreply.github.com>
2022-02-07 22:21:28 +0600

    setting up JSON server

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import api from "../api/contacts";
 import './App.css';
 import Header from "./Header";
 import AddContact from "./AddContact";
@@ -9,28 +10,45 @@ import ContactInfo from './ContactInfo'
 import ContactDelete from "./ContactDelete";
 
 function App() {
-  const LOCAL_STORAGE_KEY = "contacts";
-  const [contacts, setContacts] = useState([]);
+  //const LOCAL_STORAGE_KEY = "contacts"
+  const [contacts, setContacts] = useState([])
 
-  const addContactHandler = (contact) => {
-    setContacts([...contacts, { id: uuidv4(), ...contact }]);
+  const retrieveContacts = async () => {
+    const response = await api.get("/contacts")
+    return response.data
+  }
+
+  const addContactHandler = async (contact) => {
+    const request = {
+      id: uuidv4(),
+      ...contact
+    }
+    const response = await api.post("/contacts", request)
+    setContacts([...contacts, response.data])
   };
 
-  const removeContactHandler = (id) => {
+  const removeContactHandler = async (id) => {
+    await api.delete(`/contacts/${id}`)
     const newContactList = contacts.filter((contact) => {
-      return contact.id !== id;
+      return contact.id !== id
     });
 
-    setContacts(newContactList);
+    setContacts(newContactList)
   };
 
   useEffect(() => {
-    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (retriveContacts) setContacts(retriveContacts)
+    const getAllContacts = async () => {
+      const allContacts = await retrieveContacts()
+      if (allContacts) setContacts(allContacts)
+    }
+    getAllContacts()
+    // const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    // if (retriveContacts) setContacts(retriveContacts)
   }, [])
 
   useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
+
+    //localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts))
   }, [contacts])
 
   return (
